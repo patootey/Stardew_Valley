d607c702ea68dfae2c83e6ce5b16a8b48a391454
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2025-02-25 17:09:56 +0100

    added a menu option

--- main.cpp
+++ main.cpp
@@ -1,10 +1,54 @@
-#include <iostream>
 #include "Common.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
+void readAllNames();
+void menu();
 
-    cout << "This is the start" << endl;
+int main() {
+
+    char command;
+    menu();
+    command = lesChar("Write a command");
+
+    while (command != 'Q') {
+        switch (command) {
+        case 'A':
+            readAllNames();
+            break;
+
+        case 'S':
+            // read a singular name
+            break;
+        default:
+            menu();
+            break;
+        }
+        command = lesChar("Write a command: ");
+    }
 
     return 0;
+}
+
+void readAllNames() {
+    // Read from the file
+    string nameText;
+    ifstream namefile("names.txt");
+    if (namefile.is_open()) {
+        while (getline(namefile, nameText)) {
+            cout << nameText << '\n';
+        }
+        namefile.close();
+    }
+
+    else
+        cout << "Unable to open file";
+}
+
+void menu() {
+    cout << "This the menu choices: " << "A: Read all the villager names"
+         << "S: read a singular villager name" << endl;
 }

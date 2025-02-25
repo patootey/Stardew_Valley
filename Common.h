eade395ed7d2cc2e99033e7bdf97fe8fb12ed2e5
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2025-02-25 17:27:39 +0100

    Added the function "readInt" and changed the language to english

--- Common.h
+++ Common.h
@@ -3,14 +3,33 @@
 #include <iostream> 
 #include <iomanip>  
 #include <cctype> 
+#include <cstdlib>
 
 const int  MAXCHAR = 200; 
 
-char lesChar(const char* t)  {
-    char tegn;
+char readChar(const char* t)  {
+    char symbol;
     std::cout << t << ":  ";
-    std::cin >> tegn;  std::cin.ignore(MAXCHAR, '\n');
-    return (toupper(tegn));
+    std::cin >> symbol;  std::cin.ignore(MAXCHAR, '\n');
+    return (toupper(symbol));
 }
+
+int readInt(const char* t, const int min, const int max)  {
+    char buffer[MAXCHAR] = "";
+    int  number = 0;
+    bool wrong = false;
+
+    do {
+        wrong = false;
+        std::cout << t << " (" << min << " - " << max << "):  ";
+        std::cin.getline(buffer, MAXCHAR);
+        number = atoi(buffer);
+        if (number == 0 && buffer[0] != '0')
+        {  wrong = true;   std::cout << "\nERROR: Not an integer\n\n";  }
+    } while (wrong  ||  number < min  ||  number > max);
+
+    return number;
+}
+
 
 #endif
